096a0f69520ef105b46e009fd9e4ad484897d934
enty <38912832+kraftenty@users.noreply.github.com>
2022-09-04 19:10:43 +0900

    Add files via upload
    First Toy Project Uploaded!!

---
+++ script.js
@@ -0,0 +1,39 @@
+var numberClicked=false;
+
+function addChar(char){
+  changeClearButton(1);
+  var display=document.getElementById('display');
+
+  if((numberClicked==false&&isNaN(char)==false)||numberClicked==true){
+    if(display.value==0&&!(isNaN(char))){
+      display.value=char;
+    }
+    else{
+      display.value=display.value+char;
+    }
+  }
+  
+  if(isNaN(char)==true){
+    numberClicked=false;
+  } else{
+    numberClicked=true;
+  }
+}
+
+function calculate(){
+  var display=document.getElementById('display');
+  display.value=eval(display.value);
+}
+
+function reset(){
+  document.getElementById('display').value=0;
+  changeClearButton(0);
+}
+
+function changeClearButton(codeValue){
+  if(codeValue==0){
+    document.getElementById('clear').innerHTML="AC";
+  } else{
+    document.getElementById('clear').innerHTML="C";
+  }
+}
